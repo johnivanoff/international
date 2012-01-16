4b69d29b1238c283bfeca2c6133cb379a27967fc
John Ivanoff <john.ivanoff@gmail.com>
2012-01-16 13:15:10 -0600

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 International::Application.routes.draw do
-
-  match 'locations/' => 'locations#index', :as => :locations
+  
+  scope '(:locale)' do
+    match 'locations/' => 'locations#index', :as => :locations
+  end
   
 end
