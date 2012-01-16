aa210d5e8a9e8d04c61d461e43a6f85a9042b89b
John Ivanoff <john.ivanoff@gmail.com>
2012-01-16 15:19:15 -0600

    added steps

--- features/step_definitions/location_steps.rb
+++ features/step_definitions/location_steps.rb
@@ -17,3 +17,16 @@ Then /^(?:|I )should see "([^"]*)"$/ do |text|
     assert page.has_content?(text)
   end
 end
+
+
+Given /^I am on new location page$/ do
+  visit(new_location_path)
+end
+
+Given /^I fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
+  fill_in(field, :with => value)
+end
+
+Given /^press "([^"]*)"$/ do |button|
+  click_button(button)
+end
