bbe6a245ba8a15fcb5e215d44f322af4b430610d
John Ivanoff <john.ivanoff@gmail.com>
2012-01-16 11:35:59 -0600

    two steps passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 International::Application.routes.draw do
-  
-  get "locations/index"
 
-  match 'locations/' => 'location#index', :as => :locations
+  match 'locations/' => 'locations#index', :as => :locations
   
 end
