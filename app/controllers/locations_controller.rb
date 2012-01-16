ae72a56b18172d90696ed813f0bab79d47bbbe3a
John Ivanoff <john.ivanoff@gmail.com>
2012-01-16 11:40:57 -0600

    first test passing

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,20 @@
 class LocationsController < ApplicationController
+
+  # GET /locations
+  # GET /locations.json
   def index
+  
+    if params[:set_locale]
+      redirect_to root_path(:locale => params[:set_locale])
+    else
+      @locations = Location.all
+
+      respond_to do |format|
+        format.html # index.html.erb
+        format.json { render json: @locations }
+      end  
+    end
   end
+
 
 end
--- features/step_definitions/location_steps.rb
+++ features/step_definitions/location_steps.rb
@@ -5,3 +5,11 @@ end
 Given /^I am on the locations page$/ do
   visit(locations_path)
 end
+
+Then /^(?:|I )should see "([^"]*)"$/ do |text|
+  if page.respond_to? :should
+    page.should have_content(text)
+  else
+    assert page.has_content?(text)
+  end
+end
