38ef0494afb3fddd4baa6b33772564f671e53b89
John Ivanoff <john.ivanoff@gmail.com>
2012-01-16 15:27:15 -0600

    form working and test working to that point

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -16,5 +16,31 @@ class LocationsController < ApplicationController
     end
   end
 
+  # GET /locations/new
+  # GET /locations/new.json
+  def new
+    @location = Location.new
 
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @location }
+    end
+  end
+
+  # POST /locations
+  # POST /locations.json
+  def create
+    @location = Location.new(params[:location])
+
+    respond_to do |format|
+      if @location.save
+        format.html { redirect_to @location, notice: 'Location was successfully created.' }
+        format.json { render json: @location, status: :created, location: @location }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @location.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
 end
